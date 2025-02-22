f519b0a9213b118163d946d3524f0ac163fd4111
CHRISHARPER25 <christopher.packwood@live.com>
2025-02-23 08:41:52 +1300

    Update streamlit_app.py
    Rollback change

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ st.write("The name on your Smoothie will be", name_on_order)
 
 # ADDING IN FRUIT OPTIONS LIST FROM DATABASE
 
-#session = get_active_session()
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
